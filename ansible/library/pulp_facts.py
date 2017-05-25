8e2642312265a8b77829777a58f5da8d64ecdaaa
Mihai Ibanescu <Mihai.Ibanescu@sas.com>
2017-05-25 11:25:14 -0400

    Blacklist python-debpkgr which gets installed via pip.

--- ansible/library/pulp_facts.py
+++ ansible/library/pulp_facts.py
@@ -31,7 +31,7 @@ projects = [
     'pulp_python',
     'pulp_rpm'
 ]
-rpm_dependency_list = []
+rpm_dependency_list = set()
 # This is run using sudo, but the code is checked out in the normal user directory.
 user_homedir = pwd.getpwuid(int(os.environ['SUDO_UID'])).pw_dir
 for project in projects:
@@ -45,23 +45,23 @@ for project in projects:
         proc = subprocess.Popen(rpmspec_command, stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE, shell=True)
         stdout, stderr = proc.communicate()
-        rpm_dependency_list += [rpm.split()[0] for rpm in stdout.splitlines()]
-
-# Remove any duplicates
-rpm_dependency_list = list(set(rpm_dependency_list))
+        rpm_dependency_list.update(rpm.split()[0] for rpm in stdout.splitlines())
 
 # Replace python-lxml with python2-lxml
 # https://www.redhat.com/archives/pulp-dev/2016-December/msg00042.html
 if 'python-lxml' in rpm_dependency_list:
-    rpm_dependency_list.remove('python-lxml')
-    rpm_dependency_list.append('python2-lxml')
+    rpm_dependency_list.discard('python-lxml')
+    rpm_dependency_list.add('python2-lxml')
+
+# python-debpkgr is being installed via pip in pulp-dev.py
+rpm_dependency_list.discard('python-debpkgr')
 
 # Build the facts for Ansible
 facts = {
     'ansible_facts': {
         'pulp_nightly_repo_enabled': pulp_nightly_repo_enabled,
         'selinux_enabled': selinux_enabled,
-        'pulp_rpm_dependencies': rpm_dependency_list,
+        'pulp_rpm_dependencies': list(rpm_dependency_list),
     }
 }
 
