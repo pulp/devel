4a96615afc39e42cda3fe77614cadb4a8b174ade
Austin Macdonald <austin@redhat.com>
2018-06-04 11:44:08 -0400

    Update packages to match fedora repo
    python-django -> python2-django
    
    python-rhsm was removed, and has not yet been replaced in the upstream
    repo. Instead, I added url installs to the pre-script.

--- scripts/bootstrap-ansible.sh
+++ scripts/bootstrap-ansible.sh
@@ -3,3 +3,7 @@
 # minimal bootstrapping before kicking off ansible:
 #  install only what ansible needs to survive, or doesn't know how to do
 sudo dnf -y install python2 python2-dnf libselinux-python
+
+# Install rhsm and its deps from url since they aren't in fedora yet.
+sudo dnf -y install https://kojipkgs.fedoraproject.org//packages/subscription-manager/1.21.4/3.fc29/x86_64/subscription-manager-rhsm-certificates-1.21.4-3.fc29.x86_64.rpm
+sudo dnf -y install https://kojipkgs.fedoraproject.org//packages/subscription-manager/1.21.4/3.fc29/x86_64/python2-subscription-manager-rhsm-1.21.4-3.fc29.x86_64.rpm
