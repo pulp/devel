571476b73e78c921424b13eed9558d57ec90cf47
Mike DePaulo <mikedep333@redhat.com>
2019-07-30 15:46:43 -0400

    Do not try to install the non-existing python2-yum
    Yum never had a stable API / bindings
    
    [noissue]

--- scripts/bootstrap-ansible.sh
+++ scripts/bootstrap-ansible.sh
@@ -2,4 +2,4 @@
 
 # minimal bootstrapping before kicking off ansible:
 #  install only what ansible needs to survive, or doesn't know how to do
-sudo yum -y install python2 python2-yum libselinux-python
+sudo yum -y install python2 libselinux-python
